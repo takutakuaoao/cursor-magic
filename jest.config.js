32ccd26848e6c1fde763154ad0aef9080e4e90a2
aochan <nakinagaraodoru1013@gmail.com>
2024-09-24 17:39:55 +0900

    added esm setting for using dom on jest.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", { useESM: true }],
   },
 };
